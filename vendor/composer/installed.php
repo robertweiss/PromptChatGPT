2aec34b1151b5e44fd6f0a1005eb160a3d2f47b8
Robert Weiss <post@robertweiss.de>
2023-03-21 12:24:42 +0100

    Include vendor folder in repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'af87f3e2018bdce4b387a5891685a6eeb810dc86',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af87f3e2018bdce4b387a5891685a6eeb810dc86',
+    ),
+    'orhanerday/open-ai' => 
+    array (
+      'pretty_version' => '4.7.1',
+      'version' => '4.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd2d96d0521ef3118cc4f33a5d54718b62aedd336',
+    ),
+  ),
+);
