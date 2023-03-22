f71dc47ccf39b409a3d4f65d3e2c8f7b5fe28cf1
Robert Weiss <post@robertweiss.de>
2023-03-22 10:01:02 +0100

    Bump version

--- PromptChatGPT.info.php
+++ PromptChatGPT.info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'title' => 'PromptChatGPT',
 	'summary' => 'Send a command and/or the content of a text field to ChatGPT and save or show the result. The used model is gpt-3.5-turbo',
-	'version' => 1,
+	'version' => 2,
 	'author' => 'Robert Weiss',
 	'icon' => 'magic',
     'requires' => [
